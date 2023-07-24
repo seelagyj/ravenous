034561d7162356f6a0cc8973b11e6a6172163460
seelagyj <seelagyj@gmail.com>
2023-07-24 14:45:51 -0500

    Yelp key removed

--- src/utils/yelpAPI.js
+++ src/utils/yelpAPI.js
@@ -2,7 +2,7 @@ import SearchBar from '../components/SearchBar'
 
 
 const searchYelp = async (search, location, sort) => {
-    const apiKey = 'p-42WR60V1N_vFfg67u6Z0yqyL8IgoDIoz_2-zq621uk2Yw_NzGi4LCji_DnCmokYoKij_jp5AHvg1JUpyKJDmCILqFhuyBbxUOD58UWs9xAQvVdYaqYXU2DB9nCZHYx'
+    const apiKey = '
     const baseURL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
     const queryParams = `?term=${search}&location=${location}&sort_by=${sort}`;
         try {
